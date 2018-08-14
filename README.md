efbcabdc0f919c7a01c2993e4817396514f108c5
akokkon <42189449+akokkon@users.noreply.github.com>
2018-08-14 09:25:09 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,13 +1,47 @@
 ## README
 Docker image based on Archlinux for autoswitch hiveOS miners via API
 
+
+### install python 3.6.4
+sudo apt-get update
+
+sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+
+sudo wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz
+sudo tar xf Python-3.6.4.tar.xz
+cd Python-3.6.4
+sudo ./configure
+./sudo make altinstall
+
+sudo apt-get install libcurl4-openssl-dev
+sudo apt-get install libssl-dev
+sudo pip3.6 install --upgrade setuptools
+sudo pip3.6 install pycurl 
+sudo pip3.6 install termcolor 
+
+
 ### Create/Change VARS.py 
-Add your infomation in VARS.py
+Add your personal data in VARS.py (api keys, rig_id, wallet_id)
+
+### copy as-hive folder through ssh to hiveos (its not necessary that because it can run from any computer has installed python 3.6.4)
+
+sudo scp -r ~/Desktop/as-hive user@192.xxx.xxx.xxx:/
+chmod 770 -R as-hive
+
+### to autostart as-hive on boot 
+cd /etc/xdg/autostart
+nano as-hive.desktop
+
+[Desktop Entry]
+Type=Application
+Name=Autoswitch-hiveOS 
+Exec=sudo /as-hive/run.sh
+Icon=system-run
+X-GNOME-Autostart-enabled=true
 
 ### Start image
 Start image with docker-compose
 `docker-compose -d up`
 
 https://whattomine.com/
-
 https://hiveos.farm/
--- VARS.example.py
+++ VARS.example.py
@@ -1,8 +1,18 @@
-SECRET_KEY = 'your_secret_key'
-PUBLIC_KEY = 'your_public_key'
-RIG_ID = 'YOUR_RIG_ID'
+SECRET_KEY = 'INSERT_YOUR_SECRET_API_KEY'
+PUBLIC_KEY = 'INSERT_YOUR_PUBLIC_API_KEY'
+RIG_ID = 'INSERT_RIG_ID'
+#
+# Define the profit percentage for autoswitching
+GREATER_PROFIT = 10
+#
+# Default value is 'False'. Insert 'True' to see the list of minable coins
+DEBUG = False
+#
+# Time in seconds for the next autoswitching check
+SLEEP_TIME = 240
+#
 # Check your best server
-URL = "https://paris.hiveos.farm/worker/eypiay.php"
+URL = "https://the.hiveos.farm/worker/eypiay.php"
 
 """
 You need to pass it some parameters about your setup, and then parse it according to what you're looking for..
@@ -12,62 +22,163 @@ https://whattomine.com/
 """
 SRC = {
     'whattomine': {
-                'url': "https://whattomine.com/coins.json?utf8=%E2%9C%93&adapt_q_280x=0&adapt_q_380=0&adapt_q_fury=0&adapt_q_470=0&adapt_q_480=0&adapt_q_570=0&adapt_q_580=0&adapt_q_vega56=0&adapt_q_vega64=0&adapt_q_750Ti=0&adapt_q_1050Ti=0&adapt_q_10606=0&adapt_q_1070=0&adapt_q_1070Ti=0&adapt_q_1080=0&adapt_q_1080Ti=5&adapt_1080Ti=true&eth=true&factor%5Beth_hr%5D=175.0&factor%5Beth_p%5D=700.0&grof=true&factor%5Bgro_hr%5D=290.0&factor%5Bgro_p%5D=1050.0&x11gf=true&factor%5Bx11g_hr%5D=97.5&factor%5Bx11g_p%5D=850.0&cn=true&factor%5Bcn_hr%5D=4150.0&factor%5Bcn_p%5D=700.0&factor%5Bcn7_hr%5D=4150.0&factor%5Bcn7_p%5D=700.0&eq=true&factor%5Beq_hr%5D=3425.0&factor%5Beq_p%5D=950.0&lre=true&factor%5Blrev2_hr%5D=320000.0&factor%5Blrev2_p%5D=950.0&ns=true&factor%5Bns_hr%5D=7000.0&factor%5Bns_p%5D=950.0&factor%5Btt10_hr%5D=150.0&factor%5Btt10_p%5D=1000.0&factor%5Bx16r_hr%5D=75.0&factor%5Bx16r_p%5D=950.0&skh=true&factor%5Bskh_hr%5D=237.5&factor%5Bskh_p%5D=950.0&n5=true&factor%5Bn5_hr%5D=375.0&factor%5Bn5_p%5D=950.0&xn=true&factor%5Bxn_hr%5D=26.5&factor%5Bxn_p%5D=950.0&factor%5Bcost%5D=0.1&sort=Profit&volume=0&revenue=current&factor%5Bexchanges%5D%5B%5D=&factor%5Bexchanges%5D%5B%5D=binance&factor%5Bexchanges%5D%5B%5D=bitfinex&factor%5Bexchanges%5D%5B%5D=bittrex&factor%5Bexchanges%5D%5B%5D=cryptopia&factor%5Bexchanges%5D%5B%5D=hitbtc&factor%5Bexchanges%5D%5B%5D=poloniex&factor%5Bexchanges%5D%5B%5D=yobit&dataset=Main&commit=Calculate",
-                        'sort_key': "profitability"
-                            }
+    			'url': "https://whattomine.com/coins.json?utf8=%E2%9C%93&&adapt_q_380=0&adapt_q_fury=0&adapt_q_470=0&adapt_q_480=3&adapt_q_570=0&adapt_q_580=0&adapt_q_vega56=0&adapt_q_vega64=0&adapt_q_750Ti=0&adapt_q_1050Ti=0&adapt_q_10606=1.18&adapt_q_1070=7.93&adapt_q_1070Ti=0&adapt_q_1080=0&adapt_q_1080Ti=0&eth=true&factor[eth_hr]=247.0&factor[eth_p]=972.0&zh=true&factor[zh_hr]=320.0&factor[zh_p]=1060.0&factor[phi_hr]=45.0&factor[phi_p]=390.0&factor[cnh_hr]=2850.0&factor[cnh_p]=330.0&factor[cn7_hr]=2580.0&factor[cn7_p]=330.0&eq=true&factor[eq_hr]=3650.0&factor[eq_p]=1080.0&lre=true&factor[lrev2_hr]=285000.0&factor[lrev2_p]=1070.0&ns=true&factor[ns_hr]=7930.0&factor[ns_p]=1030.0&factor[tt10_hr]=27.0&factor[tt10_p]=450.0&x16r=true&factor[x16r_hr]=75.0&factor[x16r_p]=930.0&factor[l2z_hr]=1.35&factor[l2z_p]=360.0&factor[phi2_hr]=0.0&factor[phi2_p]=0.0&factor[xn_hr]=4.8&factor[xn_p]=360.0&factor[cost]=0.135&sort=Profit&volume=0&revenue=current&factor[exchanges][]=&factor[exchanges][]=binance&factor[exchanges][]=bitfinex&factor[exchanges][]=bittrex&factor[exchanges][]=cryptobridge&factor[exchanges][]=cryptopia&factor[exchanges][]=hitbtc&factor[exchanges][]=poloniex&factor[exchanges][]=yobit&dataset=Main&commit=Calculate",
+                'sort_key': "profitability"
+                }
         }
 
-#id_walet -> start script with the one which yout want and see the logs of this script
+#id_wallet -> start script with the one which you want and see the logs of this script
+
+"""
+Because the 'coin-tag' NICEHASH is used for several different algos (e.g. ethash, equihash, neoscrypt ...) we use as 'coin-tag' 
+and as 'algo' the name of algorithm followed by a space (e.g. 'ethash ', 'equihash ', 'neoscrypt ') 
+"""
 wallets = {
-	'NeoScrypt': {
-			'hive_name': 'neoscrypt',
-			'algo': '',
-			'miner': 'ccminer',
-			'id_wal': YOUR_ID,
-			'wallet_name': 'NeoScrypt - Nicehash'
+	'BCI': {
+			'hive_name': 'BCI',
+			'algo': 'equihash',
+			'miner': 'bminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'BCI -bci.suprnova.cc'
 			},
-	'Equihash': {
-			'hive_name': 'equihash',
+	'BTCP': {
+			'hive_name': 'BTCP',
+			'algo': 'equihash',
+			'miner': 'bminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'BTCP - stratum://btcp.suprnova.cc'
+			},
+	'BTCZ': {
+			'hive_name': 'BTCZ',
+			'algo': 'zhash',
+			'miner': 'ewbf',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'BTCZ - mine-btcz-euro.equipool.1ds.us'
+			},
+	'BTG': {
+			'hive_name': 'BTG',
+			'algo': 'zhash',
+			'miner': 'ewbf',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'BTG - btg.suprnova.cc'
+			},
+	'CMM': {
+			'hive_name': 'CMM',
+			'algo': 'equihash',
+			'miner': 'bminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'CMM - mine.vpool.io'
+			},
+	'CRC': {
+			'hive_name': 'CRC',
+			'algo': 'neoscrypt',
+			'miner': 'ccminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'CRC->BTC - stratum+tcp://neoscrypt.mine.zergpool.com'
+			},
+	'DIN': {
+			'hive_name': 'DIN',
+			'algo': 'neoscrypt',
+			'miner': 'ccminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'DIN - stratum+tcp://neoscrypt.mine.zergpool.com'
+			},
+	'ETH': {
+			'hive_name': 'ETH',
+			'algo': 'ethash',
+			'miner': 'ethminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'ETH - eth-eu.dwarfpool.com'
+			},
+	'ETC': {
+			'hive_name': 'ETC',
+			'algo': 'ethash',
+			'miner': 'ethminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'ETC - eu1-etc.ethermine.org'
+			},
+	'HUSH': {
+			'hive_name': 'HUSH',
 			'algo': 'equihash',
 			'miner': 'dstm',
-			'id_wal': YOUR_ID,
-			'wallet_name': 'Equihash - Nicehash'
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'HUSH - hush.suprnova.cc'
 			},
-	'Lyra2REv2': {
-			'hive_name': 'Lyra2REv2',
-			'algo': 'lyra2rev2',
-			'miner': 'ccminer',
-			'id_wal': YOUR_ID,
-			'wallet_name': 'Lyra2REv2 - Nicehash'
+	'KMD': {
+			'hive_name': 'KMD',
+			'algo': 'equihash',
+			'miner': 'bminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'KMD - kmd.suprnova.cc:6250'
 			},
-
-	'NIST5': {
-			'hive_name': 'NIST5',
-			'algo': 'nist5',
+	'MONA': {
+			'hive_name': 'MONA',
+			'algo': 'lyra2REv2',
 			'miner': 'ccminer',
-			'id_wal': YOUR_ID,
-			'wallet_name': 'NIST5 - NiceHash'
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'MONA - stratum+tcp://lyra2v2.mine.zergpool.com'
 			},
-	'X11Gost': {
-			'hive_name': 'X11Gost',
-			'algo': '',
-			'miner': 'ccminer',
-			'id_wal': YOUR_ID,
-			'wallet_name': 'X11Ghost - NiceHash'
+	'Equihash ': {
+			'hive_name': 'Nicehash-Equihash',
+			'algo': 'Equihash ',
+			'miner': 'bminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'Nicehash-Equihash'
 			},
-	'CryptoNight': {
-			'hive_name': 'X11Gost',
-			'algo': '',
-			'miner': 'ccminer',
-			'id_wal': YOUR_ID,
-			'wallet_name': 'CryptoNight - NiceHash'
+	'Ethash ': {
+			'hive_name': 'Nicehash-Ethash',
+			'algo': 'Ethash ',
+			'miner': 'ethminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'Nicehash-Ethash'
 			},
-	'Xevan': {
-			'hive_name': 'Xevan',
-			'algo': '',
+	'Lyra2REv2 ': {
+			'hive_name': 'Nicehash-Lyra2rev2',
+			'algo': 'Lyra2REv2 ',
 			'miner': 'ccminer',
-			'id_wal': YOUR_ID,
-			'wallet_name': 'Xevan - Supernova'
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'Nicehash-Lyra2rev2'
+			},
+	'Neoscrypt ': {
+			'hive_name': 'Nicehash-Neoscrypt',
+			'algo': 'Neoscrypt ',
+			'miner': 'ccminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'Nicehash-Neoscrypt'
+			},
+	'X16R ': {
+			'hive_name': 'Nicehash-X16R',
+			'algo': 'X16r ',
+			'miner': 'ccminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'Nicehash-X16R'
+			},
+	'RVN': {
+			'hive_name': 'RVN',
+			'algo': 'x16r',
+			'miner': 'ccminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'RVN - stratum+tcp://us.ravenminer.com'
+			},
+	'ZCL': {
+			'hive_name': 'ZCL',
+			'algo': 'equihash',
+			'miner': 'bminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'ZCL - zcl.suprnova.cc'
+			},
+	'ZEC': {
+            'hive_name': 'ZEC',
+            'algo': 'equihash',
+            'miner': 'bminer',
+            'id_wal': INSERT_WALLET_ID,
+            'wallet_name': 'ZEC - zec-eu.suprnova.cc'
+            },
+	'ZEN': {
+			'hive_name': 'ZEN',
+			'algo': 'equihash',
+			'miner': 'bminer',
+			'id_wal': INSERT_WALLET_ID,
+			'wallet_name': 'ZEN - zen.suprnova.cc'
 			}
 	}
-
